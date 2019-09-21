8f0a9f27c83daffbe3679483c64562067fbb5dce
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-09-22 01:08:03 +1000

    Add built demo

---
+++ demo/precache-manifest.754d532c53ffdddc64d4fb649a88fcda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41785eaef36aa060855dcb3f13d513d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfb1e629df06ed46c7d4",
+    "url": "/static/css/main.9954b3aa.chunk.css"
+  },
+  {
+    "revision": "4f3735c0a90d4d123e21",
+    "url": "/static/js/2.61e1a2fa.chunk.js"
+  },
+  {
+    "revision": "bfb1e629df06ed46c7d4",
+    "url": "/static/js/main.0e852607.chunk.js"
+  },
+  {
+    "revision": "d9af0f691b13185ad123",
+    "url": "/static/js/runtime~main.ea41db28.js"
+  }
+]);
